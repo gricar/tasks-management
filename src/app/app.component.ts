fd0755e0724fc09d417514f2e9d4fdd9f4fdb017
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-09-27 17:46:03 -0300

    added user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports:[HeaderComponent]
+  imports: [HeaderComponent, UserComponent]
 })
 export class AppComponent {
   title = 'task-management';
