a66a48287ab42b0b66e2c3711f612d77ab0d0204
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-10-08 14:49:31 -0300

    addded input signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
+    return 'assets/users/' + this.avatar;
   }
 }
+
+// https://github.com/mschwarzmueller/angular-complete-guide-course-resources/tree/main/code-snapshots/02-essentials/10-using-signals
