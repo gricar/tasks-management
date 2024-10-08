4c948ace036e2555134e5ba06eef28c57d7a7204
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-10-08 16:30:01 -0300

    refactor: user iteraction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
-import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent, UserComponent]
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
   title = 'task-management';
   users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
+
+  onSelectedUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
