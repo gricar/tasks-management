2ff93c6c9dbf520c347761bc837d74380cea2ccc
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-10-10 14:45:32 -0300

    refactor: module-based app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    BrowserModule,
-    AppComponent
-],
-  providers: [],
-  //bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+  ], //for non-standalone components
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule { }
