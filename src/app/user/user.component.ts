8f3d87cd57de87e48dadb58436e68f7f2f977cba
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-10-23 11:05:43 -0300

    refactor

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,7 +10,7 @@ import { type User } from './user.model';
 export class UserComponent {
   @Input({ required: true }) user!: User;
   @Input({required: true}) selected!: boolean;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
